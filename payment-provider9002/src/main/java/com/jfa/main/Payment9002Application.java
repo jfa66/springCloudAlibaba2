9a49e6a8102399e442b96eb5eb738b4ec09b5dbf
jiangfa <jiangfa@fingard.com>
2020-10-20 16:46:02 +0800

    nacos学习第2节

---
+++ payment-provider9002/src/main/java/com/jfa/main/Payment9002Application.java
@@ -0,0 +1,14 @@
+package com.jfa.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Payment9002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Payment9002Application.class,args);
+    }
+}
